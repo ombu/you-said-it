d49658c7d79425888976cf64f14318c839a928e6
Martin Rio <martin@ombuweb.com>
2011-11-17 21:49:00 -0800

    catchup commit

--- static/js/log.js
+++ static/js/log.js
@@ -4,19 +4,18 @@ dojo.require('dojo.window');
 
 dojo.addOnLoad(function() {
 
-    var log, store, viewport_size, entries_height;
+    var log, viewport_size, entries_height;
 
     log = new Log({
         container: dojo.byId('entries'),
-        paginator: InfinitePaginator
+        paginator: InfinitePaginator,
+        storeUrl: '/api/log/'
     });
-
     log.load({callback: handle_viewport_resize});
 
     function handle_viewport_resize() {
         viewport_size = dojo.window.getBox().h;
         entries_height = dojo.coords(dojo.byId('entries'), false).h;
-        //handle_scroll();
     }
 
 });
@@ -30,7 +29,7 @@ InfinitePaginator = function(domNode, callbackTop, callbackBottom) {
     this.callbackTop = callbackTop;
     this.callbackBottom = callbackBottom;
     dojo.connect(window, 'onscroll', this, this.handle_scroll);
-}
+};
 
 /**
  * Custom even to indicate paginator reached the start of the stack
@@ -38,7 +37,7 @@ InfinitePaginator = function(domNode, callbackTop, callbackBottom) {
  * @event
  */
 InfinitePaginator.prototype.onReachStart = function() {
-}
+};
 
 /**
  * Custom even to indicate paginator reached the end of the stack
@@ -46,27 +45,25 @@ InfinitePaginator.prototype.onReachStart = function() {
  * @event
  */
 InfinitePaginator.prototype.onReachEnd = function() {
-}
+};
 
 InfinitePaginator.prototype.handle_scroll = function() {
-    // did we reach the bottom?
-    reached_bottom = (window.pageYOffset + this.viewport_height -
+    reached_bottom = Boolean(window.pageYOffset + this.viewport_height -
             this.nodeHeight > 0);
     if (reached_bottom) {
         this.onReachedEnd();
     }
 
-    // did we reach the top?
-    reached_top = (window.pageYOffset == 0);
+    reached_top = Boolean(window.pageYOffset == 0);
     if (reached_top) {
         this.onReachedStart();
     }
-}
+};
 
 InfinitePaginator.prototype.refresh = function() {
     this.nodeHeight = dojo.coords(this.domNode, false).h;
     this.viewport_height = dojo.window.getBox().h;
-}
+};
 
 /**
  *  Find the node that's in the middle of the viewport
@@ -96,14 +93,15 @@ getCenterNode = function(nodeList) {
 };
 
 /**
- * TODO: document me
+ * TODO: document me better
+ * opts.storeUrl
  * opts.container
  *      container node for the log entries
  * opts.paginator
  *      the paginator object 
  */
 Log = function(opts) {
-    this.store = new dojo.store.JsonRest({target: '/api/log/'});
+    this.store = new dojo.store.JsonRest({target: opts.storeUrl});
     this.container = opts.container;
     this.paginator = new opts.paginator(this.container, this.loadDayBefore, this.loadDayBefore);
     this.entries = [];
@@ -113,20 +111,21 @@ Log = function(opts) {
 };
 
 Log.prototype.loadDayBefore = function() {
-    console.log(arguments);
-    this.load({pos: 'before'});
+    this.load({pos: 'first'});
 };
 
 Log.prototype.loadDayAfter = function() {
-    console.log(arguments);
-    this.load({pos: 'after'});
+    this.load({pos: 'last'});
 }
 
 Log.prototype.load = function(obj) {
-    date = 'foo';
-    var th = this;
-    this.store.query({date: date})
-    .then(dojo.hitch(this, this.handle_loaded))
+    var opts = {}
+    opts = dojo.mixin(opts, obj);
+    var fx =  dojo.hitch(this, function(data) {
+        this.handle_loaded(data, obj.pos);
+    });
+    this.store.query()
+    .then(fx)
     .then(function() {
         if (typeof obj.callback === 'function') {
             obj.callback();
@@ -134,23 +133,24 @@ Log.prototype.load = function(obj) {
     });
 };
 
-Log.prototype.handle_loaded = function(data) {
+Log.prototype.handle_loaded = function(data, position) {
+    if(typeof position === 'undefined') {
+        position = 'last';
+    }
+    console.log(position);
     var str = '';
     for (var i = 0, l = data.length; i < l; i++) {
         data[i][2] = new Date(data[i][2] * 1000);
         str += dojo.replace("<dt title='{2}'>{3}</dt><dd>{4}</dd>",
         data[i]);
     }
-    dojo.create('dl', {className: 'day', innerHTML: str}, this.container, 'last');
+    dojo.create('dl', {className: 'day', innerHTML: str}, this.container, position);
     this.entries = dojo.query('dt', this.container);
     this.updateMarker();
     this.paginator.refresh();
 };
 
 Log.prototype.handle_scroll = function() {
-    //window.onscroll = handle_scroll;
-    //window.onresize = handle_viewport_resize;
-
     var center_el = getCenterNode(dojo.query('dt', this.container));
     if (center_el) {
         //dojo.query('#entries dt').style('border', 'none');
@@ -158,7 +158,7 @@ Log.prototype.handle_scroll = function() {
         var date = dojo.attr(center_el, 'title');
         this.markerNode.innerHTML = date;
     }
-}
+};
 
 Log.prototype.updateMarker = function(data) {
     if (dojo.query('#marker').length == 0) {
